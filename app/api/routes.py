aa52dee726a2c7af94cbaefce3e7044c378d8fcf
fathimi-sulekha <fathimiaiml@gmail.com>
2025-01-17 04:42:26 +1300

    changed doc

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,22 +1,22 @@
-from fastapi import FastAPI
-from app.controller.year_controller import check_leap_year, check_prime
-from app.controller.string_controller import string_split_join
+from fastapi import FastAPI, APIRouter
+from app.controller.year_controller import is_leap_year, is_prime
+from app.controller.string_controller import split_and_join
 from app.dependencies.schema import YearRequest
-
+ 
 app = FastAPI()
-
+router = APIRouter()
+ 
 # Route for checking leap year
-@app.post("/check-leap-year/")
+@router.post("/check-leap-year/")
 async def check_leap_year(request: YearRequest):
-    return await check_leap_year(request)
-
+    return await is_leap_year(request.year)
+ 
 # Route for checking prime year
-@app.post("/check-prime/")
+@router.post("/check-prime/")
 async def check_prime(request: YearRequest):
-    return await check_prime(request)
-
+    return await is_prime(request.year)
+ 
 # Route for string split and join
-@app.post("/string-split-join/")
+@router.post("/string-split-join/")
 async def string_split_join(request: YearRequest):
-    return await string_split_join(request)
-
+    return await split_and_join(request.string)

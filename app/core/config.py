a767bda3b30a2dd64a699fdbeb14994ddbe908f0
fathimi-sulekha <fathimiaiml@gmail.com>
2025-01-17 19:39:18 +1300

    test commmit

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
+
 class YearRequest(BaseModel):
     year: int
     string: str
+
+   # TODO payload for each router should be designed here
